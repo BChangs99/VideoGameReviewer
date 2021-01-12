94293bde9472059322527396895dc64baac51f70
BChangs99 <yy3chang@edu.uwaterloo.ca>
2021-01-11 17:31:19 -0800

    Profile API Addition + Small Fixes - Add Private Route to Profile component - Add API functions to Profile component - Change file names - Connect redux to Profile component - Remove unncessary route to register

--- client/src/components/Profile/profile.js
+++ client/src/components/Profile/profile.js
@@ -1,58 +1,73 @@
 import React, { useState } from 'react'
 import Form from 'react-bootstrap/Form'
+import { connect } from 'react-redux'
+import { createProfile } from '../../actions/profile'
+import PropTypes from 'prop-types'
 import './profile.css'
 
-function profile(){
+function Profile({ createProfile }) {
+    const [formData, setFormData] = useState({
+        bio: '',
+        status: '',
+        location: ''
+    })
 
+    const onChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        // console.log(formData)
+        createProfile(formData)
+    }
+
+    const { location, status, bio } = formData
 
     return (
-   
         <div className="profile_base_container">
-          
             <div className="profile_form">
-            <Form>
-                <h1>Profile</h1>
-                <Form.Group >
-                    <Form.Label>User</Form.Label>
-                    <Form.Control type="email" placeholder="User" />
-                </Form.Group>
-
-                <Form.Group >
-                    <Form.Label>Status</Form.Label>
-                    <Form.Control  placeholder="Status" />
-                </Form.Group>
-
-
-                <Form.Group >
-                    <Form.Label>Bio</Form.Label>
-                    <Form.Control  placeholder="Bio" />
-                </Form.Group>
-
-
-                <Form.Group >
-                    <Form.Label>Location</Form.Label>
-                    <Form.Control placeholder="Location" />
-                </Form.Group>
-
-
-                <Form.Group >
-                    <Form.Label>Date</Form.Label>
-                    <Form.Control  placeholder="Date" />
-                </Form.Group>
-
+                <Form onSubmit={e => onSubmit(e)}>
+                    <h1>Profile</h1>
+                    <Form.Group >
+                        <Form.Label>Bio</Form.Label>
+                        <Form.Control
+                            name="bio"
+                            placeholder="Bio"
+                            value={bio}
+                            onChange={e => onChange(e)}
+                        />
+                    </Form.Group>
+                    <Form.Group >
+                        <Form.Label>Status</Form.Label>
+                        <Form.Control
+                            name="status"
+                            placeholder="Status"
+                            value={status}
+                            onChange={e => onChange(e)}
+                        />
+                    </Form.Group>
+                    <Form.Group >
+                        <Form.Label>Location</Form.Label>
+                        <Form.Control
+                            name="location"
+                            placeholder="Location"
+                            value={location}
+                            onChange={e => onChange(e)}
+                        />
+                    </Form.Group>
+                    <div >
+                        <button type="submit">Create/Update</button>
+                    </div>
                 </Form>
-
             </div>
         </div>
-
-
-
-
     )
-
-
-
 }
 
-export default profile
+Profile.propTypes = {
+    createProfile: PropTypes.func.isRequired,
+}
+
+export default connect(null, { createProfile })(Profile)
 
